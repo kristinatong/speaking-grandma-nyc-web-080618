b740e87a614caa252689bc7c9a404e783fff321d
Kristina Tong <kristina.tong@gmail.com>
2018-07-29 14:54:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,9 @@
 def speak_to_grandma(string)
   if string == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
+  elsif string.upcase == string
+    'NO, NOT SINCE 1938!'
+  else
+    'HUH?! SPEAK UP, SONNY!'
   end
 end
